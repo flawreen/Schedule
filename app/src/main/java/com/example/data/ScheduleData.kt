c84c054c4d1f47019388a6da8929e0ae0471d0e2
Florin <83332450+flawreen@users.noreply.github.com>
2023-01-11 18:45:54 +0200

    orar

--- app/src/main/java/com/example/data/ScheduleData.kt
+++ app/src/main/java/com/example/data/ScheduleData.kt
@@ -1,87 +1,67 @@
 package com.example.data
-
 import com.example.model.Materie
 
 object ScheduleData {
     val materii: List<Materie> = listOf(
         Materie(
             "Luni",
-            "Anatomie Curs",
-            "IOR Amf. Palade",
-            "8:00 - 10:00"
-        ),
-        Materie(
-            "Luni",
-            "Biochimie Curs",
-            "IOR Amf. Palade",
+            "Algebra",
+            "1",
             "10:00 - 12:00"
         ),
         Materie(
-            "Luni",
-            "Info LP",
-            "Corpul M Timpuri Noi",
-            "14:00 - 16:00"
-        ),
-        Materie(
-            "Luni",
-            "Sport [SI]",
-            "Camin UTM",
+            "Marti",
+            "ASC Lab [SI]",
+            "321",
             "16:00 - 18:00"
         ),
         Materie(
-            "Marti",
-            "Anatomie LP",
-            "IOR Lab A3",
-            "8:00 - 10:00"
+            "Miercuri",
+            "Python Seminar [SI]",
+            "120",
+            "10:00 - 12:00"
         ),
         Materie(
-            "Marti",
-            "Bio. Cel. LP",
-            "Corp Sincai",
+            "Miercuri",
+            "ASC Curs",
+            "Pompeiu",
             "14:00 - 16:00"
         ),
         Materie(
             "Miercuri",
-            "Info Curs [SI]",
-            "",
-            "8:00 - 10:00"
+            "Sport",
+            "Limbi Straine",
+            "16:00 - 18:00"
         ),
         Materie(
-            "Miercuri",
-            "Bio. Cel. Curs",
-            "IOR Amf. Et. 2",
+            "Joi",
+            "Gandire Critica",
+            "1",
             "10:00 - 12:00"
         ),
         Materie(
             "Joi",
-            "Engleza [SP]",
-            "IOR Amf. Et. 1",
-            "9:00 - 10:00"
-        ),
-        Materie(
-            "Joi",
-            "Anatomie LP",
-            "IOR Lab A1",
+            "Analiza",
+            "5",
             "12:00 - 14:00"
         ),
         Materie(
-            "Joi",
-            "Biochimie LP",
-            "IOR Lab 1",
-            "18:00 - 20:00"
-        ),
-        Materie(
             "Vineri",
-            "Genetica Curs",
-            "IOR Amf. Et. 2",
-            "9:00 - 10:00"
-        ),
-        Materie(
-            "Vineri",
-            "Genetica LP",
-            "Ior Lab. Microb V",
+            "Python Lab [SI]",
+            "221-A",
             "10:00 - 12:00"
+        ),
+        Materie(
+            "Vineri",
+            "Python Curs",
+            "Pompeiu",
+            "12:00 - 14:00"
+        ),
+        Materie(
+            "Vineri",
+            "ASC Seminar [SP]",
+            "Pompeiu",
+            "14:00 - 16:00"
         )
-
     )
 }
